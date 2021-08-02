bbd947317821518321afb3588a411bc6a9435e67
k <kvanst3@gmail.com>
2021-08-02 18:00:02 +0200

    Int solution

--- palindrome_num.py
+++ palindrome_num.py
@@ -1,7 +1,20 @@
-class Solution:
+class StrSolution:
     """Solution converting to string"""
     def isPalindrome(self, x: int) -> bool:
         rev_x = str(x)[::-1]
         if rev_x == str(x):
             return True
 
+class IntSolution:
+    def isPalindrome(self, x: int) -> bool:
+        if x >= 0:
+            rev_num = 0
+            ori_num = x
+            while x > 0:
+                rev_num = (rev_num * 10) + x%10
+                x = x//10
+            if rev_num == ori_num:
+                return True
+
+x = IntSolution()
+print(x.isPalindrome(121))

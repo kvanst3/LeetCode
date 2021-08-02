efa3cc34639ddffd23689b67c70f786555e2a58c
k <kvanst3@gmail.com>
2021-08-02 15:56:52 +0200

    working solution

--- reverse_int.py
+++ reverse_int.py
@@ -1,7 +1,16 @@
 class Solution:
     def reverse(self, x: int) -> int:
-        if x >= -2^31 & x <=(2^31) -1:
-            rev_num = 0
-            while x > 0:
-                rev_num = (rev_num * 10) + x%10
-                x = x//10
+        pos = True if x > 0 else False
+        rev_num = 0
+        x = abs(x)
+        while x > 0:
+            rev_num = (rev_num * 10) + x%10
+            x = x//10
+        if rev_num >= -2**31 & rev_num <=(2**31) -1:
+            return rev_num if pos else rev_num * -1
+        else:
+            return 0
+
+
+x = Solution()
+print(x.reverse(x=-321))

77a432009b928539d1ced6586b37fb2d41c7ecec
k <kvanst3@gmail.com>
2021-08-08 12:13:22 +0200

    adding at tail

---
+++ linked_list.py
@@ -0,0 +1,22 @@
+class Node():
+
+    def __init__(self, val):
+        self.value = val
+        self.next = None
+
+class MyLinkedList():
+
+    def __init__(self):
+        self.head = None
+        self.size = 0
+
+    def AddAtTail(self, val):
+        new_node = Node(val)
+        curr = self.head
+        if curr == None:
+            self.head = new_node
+        else:
+            while curr.next is not None:
+                curr = curr.next
+            curr.next = new_node
+        self.size += 1

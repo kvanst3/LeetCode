a51e0dc6c90da0fe4d6217a47ebf90dd756dd75b
k <kvanst3@gmail.com>
2021-08-08 16:02:43 +0200

    add at index n

--- linked_list.py
+++ linked_list.py
@@ -1,6 +1,6 @@
 class Node():
 
-    def __init__(self, val):
+    def __init__(self, val: int):
         self.value = val
         self.next = None
 
@@ -10,7 +10,7 @@ class MyLinkedList():
         self.head = None
         self.size = 0
 
-    def AddAtTail(self, val):
+    def AddAtTail(self, val: int):
         new_node = Node(val)
         curr = self.head
         if curr == None:
@@ -21,13 +21,46 @@ class MyLinkedList():
             curr.next = new_node
         self.size += 1
 
-    def AddAtHead(self, val):
+    def AddAtHead(self, val: int):
         new_node = Node(val)
         new_node.next = self.head
         self.head = new_node
 
         self.size += 1
 
-    def get(self, index):
+    def get(self, index: int):
         if index < 0 or index >= self.size:
-            
+            return -1
+        if self.head is None:
+            return -1
+        curr = self.head
+        for _ in range(index):
+            curr = curr.next
+        return curr.value
+
+    def addAtIndex(self, index: int, val: int):
+        """
+        Add a node of value val before the index-th node in the linked list. 
+        If index equals to the length of linked list, the node will be appended to the end of linked list. 
+        If index is greater than the length, the node will not be inserted.
+        """
+        if index < 0 or index >= self.size:
+            return 
+        if index == 0:
+            self.AddAtHead(val)
+        elif index + 1 == self.size:
+            self.AddAtTail(val)
+        else:
+            curr = self.head
+            for _ in index -1:
+                curr = curr.next
+            new_node = Node(val)
+            new_node.next = curr.next
+            curr.next = new_node
+            self.size += 1
+        
+
+    def deleteAtIndex(self, index: int) -> None:
+        """
+        Delete the index-th node in the linked list, if the index is valid.
+        """

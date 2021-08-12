24ecdb38062cb7f45ca38e33ad58f6533c25a62a
k <kvanst3@gmail.com>
2021-08-12 11:21:41 +0200

    hash solution

--- haystack_needle.py
+++ haystack_needle.py
@@ -4,5 +4,14 @@ class Solution:
             return haystack.find(needle)
         return 0
 
+    def strHash(self, haystack, needle):
+        n, h = len(needle), len(haystack)
+        hash_n = hash(needle)
+        for i in range(h-n+1):
+            if hash(haystack[i:i+n]) == hash_n:
+                return i
+        return -1
+
+
 x = Solution()
-print(x.strStr('hello', 'll'))
+print(x.strHash('hello', 'll'))

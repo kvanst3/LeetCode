1fd655d792bb2b61a1cd9840c223d5029a971482
k <kvanst3@gmail.com>
2021-08-13 15:11:35 +0200

    recursive solution

--- plus_one.py
+++ plus_one.py
@@ -1,9 +1,19 @@
 class Solution:
-    def plusOne(self, digits: List[int]) -> List[int]:
+    def plusOne(self, digits):
         strings = [str(digit) for digit in digits]
         a_string = "".join(strings)
         an_integer = int(a_string) + 1
         d = [int(a) for a in str(an_integer)]
         return d
 
+    def plusOneFaster(self, digits):
+        if digits[-1] == 9:
+            if len(digits) == 1:  # Already a 9
+                return [1, 0]
+            return self.plusOneFaster(digits[:-1]) + [0]
+        else:
+            digits[-1] += 1
+        return digits
 
+x = Solution()
+print(x.plusOneFaster([1,9,9]))

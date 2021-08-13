77d0f48e8b6baf88b2d0ecb212a52d02a107531a
k <kvanst3@gmail.com>
2021-08-13 11:46:30 +0200

    space (O)1

--- max_subarray.py
+++ max_subarray.py
@@ -5,7 +5,13 @@ class Solution:
             dp_table[i] = max(dp_table[i-1] + num, num)
         return max(dp_table)
 
-
+    def maxProfit(self, nums):
+        cur_max = glo_max = nums[0]
+        for num in nums[1:]:
+            cur_max = max(num, cur_max + num)
+            glo_max = max(cur_max, glo_max)
+        return glo_max
 
 x = Solution()
 print(x.maxSubArray([-2,1,-3,4,-1,2,1,-5,4]))
+print(x.maxProfit([-2,1,-3,4,-1,2,1,-5,4]))
